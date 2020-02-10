742394e431a90e370d5b0b5f97e121595c8d40bb
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-10 20:26:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,6 +6,7 @@ class Dog
  
   def initialize(name)
     @name = name
+    self.save
   end
   
   def self.all
@@ -22,7 +23,7 @@ class Dog
     end
   end
   
-  def self.save 
+  def save 
     @@all << self
   end 
 end
